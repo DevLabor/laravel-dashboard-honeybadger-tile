fab98a17fb8a98bd7af8b35ec31045235177573a
Dennis Pakebusch <dennis.pakebusch@gmail.com>
2023-11-16 12:39:51 +0100

    overview component

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -2,36 +2,29 @@
     :position="$position"
     :fade="false"
     :refresh-interval="$refreshIntervalInSeconds">
-    <div class="absolute inset-0 @if($unresolvedFaults || $offlineSites) bg-error @endif p-4">
+    <div class="absolute inset-0 p-4">
         <div class="grid grid-rows-auto-1 h-full">
             @isset($title)
                 <div class="tile-heading">
-                    <h1 class="uppercase text-center font-bold @if($unresolvedFaults || $offlineSites) text-white @endif">
+                    <h1 class="uppercase text-center font-bold">
                         {{ $title }}
                     </h1>
                 </div>
             @endisset
             <div class="flex">
-                <div class="grid grid-rows-auto-1 w-1/2 h-full">
-                    @isset($description_faults)
-                    <h2 class="uppercase text-xs @if($unresolvedFaults || $offlineSites) text-white @endif text-opacity-50 text-center">
-                        {{ $description_faults }}
+                @foreach([
+                    [$unresolvedFaults, $description_faults], 
+                    [$offlineSites, $description_offline]
+                ] as $data)
+                <div class="grid grid-rows-auto-1 w-1/2 h-full rounded @if($data[0]) bg-error @endif">
+                    <h2 class="uppercase text-xs @if($data[0]) text-white @endif text-opacity-50 text-center">
+                        @isset($data[1]) {{ $data[1] }} @endisset
                     </h2>
-                    @endisset
                     <div class="flex items-center justify-center">
-                        <p class="text-4xl text-center font-bold @if($unresolvedFaults || $offlineSites) text-white @endif">{{ $unresolvedFaults }}</p>
+                        <p class="text-4xl text-center font-bold @if($data[0]) text-white @endif">{{ $data[0] }}</p>
                     </div>
                 </div>
-                <div class="grid grid-rows-auto-1 w-1/2 h-full">
-                    @isset($description_offline)
-                    <h2 class="uppercase text-xs @if($unresolvedFaults || $offlineSites) text-white @endif text-opacity-50 text-center">
-                        {{ $description_offline }}
-                    </h2>
-                    @endisset
-                    <div class="flex items-center justify-center">
-                        <p class="text-4xl text-center font-bold @if($unresolvedFaults || $offlineSites) text-white @endif">{{ $offlineSites }}</p>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
